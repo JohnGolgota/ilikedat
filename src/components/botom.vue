0ff37f3d80307ff36f2c656cd4a3d4d4e53b2014
JohnGolgota <js684new@gmail.com>
2023-12-16 18:24:47 -0500

    fuj

---
+++ src/components/botom.vue
@@ -0,0 +1,33 @@
+<template>
+    <button @click="increment">
+        +
+    </button>
+    <div>
+        {{ count }}
+    </div>
+    <button @click="decrement">
+        -
+    </button>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+const count = ref(0)
+const increment = () => count.value++
+const decrement = () => count.value--
+</script>
+<style scoped>
+button {
+    background-color: #fff;
+    border: 1px solid #999;
+    border-radius: 3px;
+    color: #666;
+    font-size: 18px;
+    padding: 5px 10px;
+}
+
+div {
+    color: #666;
+    font-size: 24px;
+    padding: 5px 10px;
+}
+</style>
